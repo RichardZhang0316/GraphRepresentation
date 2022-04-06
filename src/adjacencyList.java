831c1b42c7053254565b3094a3e1d9b87355a5bd
RichardZhang0316 <zhank20@wfu.edu>
2022-04-06 16:46:17 -0400

    232312341890

--- src/adjacencyList.java
+++ src/adjacencyList.java
@@ -1,3 +1,48 @@
 public class adjacencyList {
-    
+    EdgeNode[] graph;
+
+    private class EdgeNode{
+        int vertex_id;
+        int weight;
+
+        EdgeNode prev;
+        EdgeNode next;
+    }
+
+    public adjacencyList(int num_vertices){
+        this.graph=new EdgeNode[num_vertices];
+    }
+
+    //add an edge to the graph
+    public void addEdge(int from, int to, int weight){
+        EdgeNode newNode=new EdgeNode();
+        newNode.weight=weight;
+        newNode.vertex_id=to;
+
+        newNode.next=this.graph[from];
+        if(this.graph[from]!=null){
+            this.graph[from].prev=newNode;
+        }
+        this.graph[from]=newNode;
+        newNode.prev=null;
+    }
+
+    public int[] getNeighbors(int v){
+       // loop to figure out the length of the linkedList
+        int count = 0;
+        EdgeNode tmp=this.graph[v];
+        while(tmp != null){
+            count++;
+            tmp=tmp.next;
+        }
+        int[] neighbors = new int[count];
+
+        count=0;
+        while(tmp != null){
+            neighbors[count]=tmp.vertex_id;
+            count++;
+            tmp=tmp.next;
+        }
+        return neighbors;
+    }
 }
